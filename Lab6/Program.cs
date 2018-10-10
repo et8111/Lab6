9224d773036c919a33e4228d75f8338816c4eef6
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-10 11:46:11 -0400

    Add project files.

---
+++ Lab6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        public static void RGENERATOR(Random r, int dieSides)
+        {
+            int r1 = (r.Next(1, dieSides + 1));
+            int r2 = (r.Next(1, dieSides + 1));
+            string msg = "";
+            if (r1 + r2 == 7)
+                msg = "Seven! *snaps fingers*";
+            else if (r1 == 1 && r2 == 1)
+                msg = "SNAKE EYES.";
+            else if (r1 == 6 && r2 == 6)
+                msg = "Boxcar.";
+            Console.WriteLine("DICE1".PadRight(7) + "DICE2".PadRight(7));
+            Console.WriteLine(r1.ToString().PadRight(7) + r2.ToString().PadRight(7)+ msg);
+
+        }
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("ENTER SIDES: ");
+            int dieSides = int.Parse(Console.ReadLine());
+            bool s = true;
+            while (s)
+            {
+                RGENERATOR(r, dieSides);
+                s = (Console.ReadLine() == "n") ? false : true;
+            }
+
+            
+        }
+    }
+}
